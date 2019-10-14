cc618e48716873872b92449a71cb351cce8d3dd6
Jayvin <jayvin@126.com>
2019-10-14 18:43:55 +0800

    firstCommit

---
+++ src/main/java/domain/User.java
@@ -0,0 +1,11 @@
+package domain;
+
+/*
+ * @author Jayvin
+ * @date 2019/10/11 20:50
+ */
+public class User {
+    private String username; //用户名
+    private Integer age;
+    private String address;
+}
