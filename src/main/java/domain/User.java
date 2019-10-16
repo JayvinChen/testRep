247f77ad4fecb6ac39eae238906f1a2becf46b52
Jayvin <jayvin@126.com>
2019-10-16 12:33:19 +0800

    add age getter and setter

--- src/main/java/domain/User.java
+++ src/main/java/domain/User.java
@@ -8,5 +8,12 @@ public class User {
     private String username; //用户名
     private Integer age; //年龄
     private String address;///hghhghg
-    /////
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 }
